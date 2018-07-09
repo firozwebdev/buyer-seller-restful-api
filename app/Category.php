f94a0c28e7a54b53a5da118a1b3723ab6eebd898
sabuzwebdev <frsmgt@gmail.com>
2018-07-09 12:35:08 +0600

    Model Relationship is completed

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Product;
 
 class Category extends Model
 {
     protected $fillable = [
         'name', 'description'
     ];
+
+    public function products(){
+        return $this->belongsToMany(Product::class);
+    }
 }
