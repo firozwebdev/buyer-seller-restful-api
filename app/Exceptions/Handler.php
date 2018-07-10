534ae2873d470d797f77106815e9aab0108e6e40
sabuzwebdev <frsmgt@gmail.com>
2018-07-10 21:10:25 +0600

    Errors and exceptions are handled with laravel exception  handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,22 @@
 namespace App\Exceptions;
 
 use Exception;
+use App\Traits\ApiResponser;
+
+use Illuminate\Database\QueryException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -48,6 +60,49 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof ValidationException){
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+        if($exception instanceof ModelNotFoundException){
+            $modelName = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("Does not exists any $modelName with the specified identificator",404);
+        }
+
+        if($exception instanceof AuthenticationException) {
+            return $this->errorResponse('Unauthenticated !', 401);
+        }
+        if($exception instanceof AuthorizationException) {
+            return $this->errorResponse($exception->getMessage(), 403);
+        }
+        
+        if($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('The specified url can not be found', 404);
+        }
+
+        if($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('The specified method for the request can not be found', 405);
+        }
+        if($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+        if($exception instanceof QueryException) {
+            $errorCode = $exception->errorInfo[1];
+            if($errorCode == 1451){
+                return $this->errorResponse('Can not remove this resource permanently. It is related with any other resource',409);
+            }
+           
+        }
+
+        return $this->errorResponse('Unexpected Exception. Try later', 500);
+
+
         return parent::render($request, $exception);
+    }
+
+
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request){
+        $errors = $e->validator->errors()->getMessages();
+
+        return $this->errorResponse($errors,422);
     }
 }
--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -18,6 +18,7 @@ class UserController extends ApiController
         $users = User::all();
 
         return $this->showAll($users);
+       
     }
 
  
