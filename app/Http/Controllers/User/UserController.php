eb025d1f893bd0c83f7c3df8a6051ce900e2bc7b
sabuzwebdev <frsmgt@gmail.com>
2018-07-11 10:57:47 +0600

    Implicit model was binded with global scope with respective model

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -55,9 +55,8 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        $user = User::findOrFail($id);
         return $this->showOne($user);
     }
 
@@ -69,9 +68,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        $user = User::findOrFail($id);
+       
         $rules = [
             'name' => 'required',
             'email' => 'required|email|unique:users,email,'.$user->id,
@@ -114,9 +113,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::findOrFail($id);
+       
         $user->delete();
 
         return $this->showOne($user);
