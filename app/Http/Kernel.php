1fa0a34bd643a4251ce934f46441aae8232138ab
sabuzwebdev <frsmgt@gmail.com>
2018-08-01 20:36:57 +0600

    Adding Middleware for rating limit response

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,8 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'signature:X-Application',
+            'throttle:10,1',
             'bindings',
         ],
     ];
@@ -57,6 +58,7 @@ class Kernel extends HttpKernel
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'throttle' => \App\Http\Middleware\CustomThrottleRequests::class,
+        'signature' => \App\Http\Middleware\SignatureMiddleware::class,
     ];
 }
